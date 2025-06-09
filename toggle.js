f2a389d3c1d40443b49fa18584bbf809ba481e80
MikhaelAdrielPG <mikhael.adriel.pratama.gana@gmail.com>
2025-06-09 23:21:33 +0700

    add dark mode feature

--- toggle.js
+++ toggle.js
@@ -16,3 +16,24 @@ function toggleShow(
   button.textContent = allVisible ? showText : hideText;
   button.setAttribute("aria-expanded", !allVisible);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("darkModeToggle");
+
+  if (localStorage.getItem("darkMode") === "enabled") {
+    document.documentElement.classList.add("dark-mode");
+    toggleBtn.innerHTML = '<i class="bi bi-sun"></i>';
+  }
+
+  toggleBtn.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark-mode");
+
+    if (document.documentElement.classList.contains("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+      toggleBtn.innerHTML = '<i class="bi bi-sun"></i>';
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+      toggleBtn.innerHTML = '<i class="bi bi-moon"></i>';
+    }
+  });
+});
